cd1a8f28aefc2b0b6b35eb97012bfe74b1a3ab4a
KondratenkoS <sergeikhell@gmail.com>
2024-02-06 15:08:56 +0200

    trying routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,83 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+/* ЗАДАЧКИ НА МАРШРУТЫ */
+
+/* тест сообщение */
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello Hell!';
+});
+
+/* Сделайте так, чтобы при обращении на адрес /test в браузер выводилось какое-нибудь сообщение. */
+Route::get('/test', function () {
+    return 'Это тестовое сообщение на URI /test';
+});
+
+/* Сделайте так, чтобы при обращении на адрес /dir/test в браузер выводилось какое-нибудь сообщение. */
+Route::get('/dir/test', function () {
+    return 'Это тестовое сообщение на URI /dir/test';
+});
+
+/* Сделайте маршрут, обрабатывающий адреса вида /user/:name, где вместо :name может быть любая строка. */
+Route::get('/user/{name}', function ($name) {
+    return "В {name} находится - " . $name;
+});
+
+/* Сделайте маршрут, обрабатывающий адреса вида /user/:surname/:name/, где параметры задают имя и фамилию юзера. */
+Route::get('/username/{surname}/{name}', function ($surname, $name) {
+    return "Имя и фамилия пользователя - " . $surname . ' ' . $name;
+});
+
+/* Пусть дан адрес вида /city/:city, где в параметре будет задаваться город.
+ Сделайте так, чтобы город был необязательным параметром и по умолчанию имел значение Chernigiv. */
+Route::get('/city/{city?}', function ($city = 'Chernigiv') {
+    return 'Название города - ' . $city;
+});
+
+/* Сделайте маршрут вида /user/:id, где вместо :id должно быть число. */
+Route::get('/num/{id}', function ($id) {
+    return $id;
+})->where('id', '[0-9]+');
+
+/* Сделайте маршрут вида /user/:id/:name, где вместо :id должно быть число,
+ а вместо :name - строка, состоящая из маленьких латинских букв количеством более 2-х. */
+Route::get('/user/{name}/{id}', function ($name, $id) {
+    return 'Под номером - ' . $id . ' пользователь под именем - ' . $name;
+})->where('id', '[0-9]+')->where('name', '[a-z].+');
+
+/* Сделайте маршрут вида /posts/:date, где вместо :date должна быть дата в формате год-месяц-день. */
+Route::get('/post/{date}', function ($date) {
+    return 'Дата - ' . $date;
+})->where('date', '\d{4}-\d{2}-\d{2}');
+
+/* Сделайте маршрут вида /:year/:month/:day, где вместо :year должен быть год,
+ вместо :month - месяц, вместо :day - день. */
+Route::get('/post/{year}/{month}/{day}', function ($year, $month, $day) {
+    return 'Дата - ' . $year . '-' . $month . '-' . $day;
+})  ->where('year', '\d{4}')
+    ->where('month', '\d{2}')
+    ->where('day', '\d{2}');
+
+/* Сделайте маршрут вида /users/:order, где вместо :order должно быть одно из значений:
+ 'name', 'surname' или 'age'. */
+Route::get('/users/{order}', function ($order){
+    return $order;
+})->where('order', '(name)|(surname)|(age)');
+
+/* Сделайте маршрут вида /number/:id, где вместо :id должно быть число. */
+Route::get('/number/{id}', function ($id){
+    return $id;
+})->whereNumber('id');
+
+/* Сделайте маршрут вида /city/:name, где вместо :name должны быть буквы. */
+Route::get('/cityname/{name}', function ($name){
+    return $name;
+})->whereAlpha('name');
+
+/* Наложите глобальное ограничение на параметр slug. Пусть он может содержать
+буквы и цифры, а также дефис и подчеркивание. */
+Route::get('/global/{slug}', function ($slug){
+    return $slug;
 });
